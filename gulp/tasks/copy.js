f2b5bb864b139cd52b33555629e9eb775344d668
Keccap <alexkeccap@ya.ru>
2019-06-11 11:42:41 +0300

    copy root file to build, bg refactor

--- gulp/tasks/copy.js
+++ gulp/tasks/copy.js
@@ -2,33 +2,41 @@ const gulp = require('gulp');
 const config = require('../config');
 
 
-
-
-gulp.task('copy:img', () => {
-	return gulp
-		.src(config.src.img + '/**/*.*')
-		.pipe(gulp.dest(config.dest.img));
+gulp.task('copy:root', () => {
+    return gulp
+    .src(config.src.root + '/*.*')
+    .pipe(gulp.dest(config.dest.root));
 });
 
+gulp.task('copy:img', () => {
+    return gulp
+    .src(config.src.img + '/**/*.*')
+    .pipe(gulp.dest(config.dest.img));
+});
 
 
 gulp.task('copy:img:watch', cb => {
-	gulp.watch(config.src.img + '/**/*.*', gulp.series('copy:img'));
-	cb();
+    gulp.watch(config.src.img + '/**/*.*', gulp.series('copy:img'));
+    cb();
+});
+
+gulp.task('copy:root:watch', cb => {
+    gulp.watch(config.src.root + '/*.*', gulp.series('copy:root'));
+    cb();
 });
 
 
-
-
 gulp.task('copy', cb => {
-	gulp.parallel(
-		'copy:img'
-	)(cb);
+    gulp.parallel(
+        'copy:img',
+        'copy:root'
+    )(cb);
 });
 
 
 gulp.task('copy:watch', cb => {
-	gulp.parallel(
-		'copy:img:watch'
-	)(cb);
+    gulp.parallel(
+        'copy:img:watch',
+        'copy:root:watch'
+    )(cb);
 });
